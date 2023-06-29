c621637ff324486c550b8a0012dca896a0b3815b
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-06-29 13:11:02 -0400

    removed filter

--- functions/dbConnect.js
+++ functions/dbConnect.js
@@ -7,3 +7,15 @@ export const db = client.db('HoneyHoleDB')
 
 
 
+// export async function getHoneyHoleByFilter (req, res) {
+
+//   const {filterValue} = req.params;
+
+//   const collection = await collectionClient
+//     .find( {"county": filterValue} )
+//     .toArray()
+//     .catch(err => res.status(500).json(errMessage));
+
+//   console.table( {collection} );
+//   res.status(200).json(collection);
+// }
